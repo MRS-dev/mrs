1e3b6b2734a51c03e50d5de90fa29d0bc5ad9d74
stephen leroy <stleroy@et.esiea.fr>
2025-06-02 20:15:19 +0200

    try add fetchoptions

--- apps/admin/src/lib/authClient.ts
+++ apps/admin/src/lib/authClient.ts
@@ -9,12 +9,18 @@ import { AuthType } from "../../../server/src/lib/auth";
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
   basePath: "/api/auth", // ← ici
+  fetchOptions: {
+    credentials: "include",
+  },
   plugins: [twoFactorClient(), inferAdditionalFields<AuthType>()],
 });
 
 export const adminClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
   basePath: "/api/auth", // ← ici
+  fetchOptions: {
+    credentials: "include",
+  },
   plugins: [
     adminClientPlugin(),
     twoFactorClient(),
