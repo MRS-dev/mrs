86f92566155c7ceca8960ba338744604939722bc
lympe <matthe.leroy@gmail.com>
2025-06-02 20:50:20 +0200

    refactor: simplify apiClient configuration and enhance cookie handling attributes

--- apps/pro/src/lib/apiClient.ts
+++ apps/pro/src/lib/apiClient.ts
@@ -3,10 +3,7 @@ import { AppType } from "../../../server/src/index";
 import { hc } from "hono/client";
 
 export const client = hc<AppType>(process.env.NEXT_PUBLIC_SERVER_URL!, {
-  fetch: ((input, init) => {
-    return fetch(input, {
-      ...init,
-      credentials: "include", // Required for sending cookies cross-origin
-    });
-  }) satisfies typeof fetch,
+  init: {
+    credentials: "include", // pour inclure les cookies si nécessaire
+  },
 });
