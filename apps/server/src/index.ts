b3d448c75807433e32e37873c7ae5d0cda4a70e5
stephen leroy <stleroy@et.esiea.fr>
2025-06-02 19:28:13 +0200

    fix conf cors

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -35,11 +35,7 @@ const app = new Hono<HonoType>()
   .use(
     "/api",
     cors({
-      origin: [
-        process.env.ADMIN_FRONTEND_URL!,
-        process.env.PRO_FRONTEND_URL!,
-        process.env.PATIENT_FRONTEND_URL!,
-      ],
+      origin: [process.env.ADMIN_FRONTEND_URL!, process.env.PRO_FRONTEND_URL!],
       allowMethods: ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
       allowHeaders: ["Content-Type", "Authorization"],
       credentials: true,
